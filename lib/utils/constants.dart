6a19d2f045c35e078df3c34c7764176a36b03d4d
Aman Chaudhary <amanchaudhary@Amans-MacBook-Pro.local>
2019-12-17 23:56:08 +0530

    Initial Commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+
+  static final String HEADLINE_NEWS_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=0ae83f767977429f9f008285f0e39d7f';
+
+  static final String NEWS_PLACEHOLDER_IMAGE_ASSET_URL = 'assets/placeholder.png';
+
+}
